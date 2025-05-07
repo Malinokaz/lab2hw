352b3987e3ab2ff1da5f91979497d822d52651c4
Vladimir <malinokaz@ya.ru>
2025-05-07 22:22:57 +0300

    fixed conflict

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <string>    
-
-int main() {
-    std::string user_name;            // username
-    std::cout << "Enter your name: "; // asking to enter name
-    std::cin >> user_name;            // input username
-    std::cout << "Hello world from @" << user_name << std::endl; // print greetings
-    return 0;                         // end of program
+#include <string>
+int main()
+{
+  std::string user_name;            // имя пользователя
+  std::cout << "Enter your name: "; // вывод запроса
+  std::cin >> user_name;            // ввод имени
+  std::cout << "Hello world from @" << user_name
+            << std::endl; // вывод приветствия
+  return 0;               // завершение программы
 }
