60a63a57c7a46fde4b037fd74cca50e62548f6bd
Vladimir <malinokaz@ya.ru>
2025-05-07 22:13:40 +0300

    added comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <string>
+#include <string>    
 
-int main()
-{
-    std::cout << "Enter your name: ";
-    std::string user_name;
-    std::cin >> user_name;
-
-    std::cout << "Hello world from @" << user_name << std::endl;
-
-    return 0;
+int main() {
+    std::string user_name;            // имя пользователя
+    std::cout << "Enter your name: "; // вывод запроса
+    std::cin >> user_name;            // ввод имени
+    std::cout << "Hello world from @" << user_name << std::endl; // вывод приветствия
+    return 0;                         // завершение программы
 }
