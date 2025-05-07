49a5ba01688bab04ad8e47c74417c110d6e44e74
Malinokaz <98411157+Malinokaz@users.noreply.github.com>
2025-05-07 22:27:29 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>    
 
 int main() {
-    std::string user_name;            // имя пользователя
-    std::cout << "Enter your name: "; // вывод запроса
-    std::cin >> user_name;            // ввод имени
-    std::cout << "Hello world from @" << user_name << std::endl; // вывод приветствия
-    return 0;                         // завершение программы
+    std::string user_name;            // username
+    std::cout << "Enter your name: "; // asking to enter name
+    std::cin >> user_name;            // input username
+    std::cout << "Hello world from @" << user_name << std::endl; // print greetings
+    return 0;                         // end of program
 }
