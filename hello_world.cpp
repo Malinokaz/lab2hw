3d54ad333ba2a0071d3ee3cc28c871c7d61c4f55
Malinokaz <98411157+Malinokaz@users.noreply.github.com>
2025-05-30 04:26:43 +0300

    edited codestyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
-int main()
+
+int
+main()
 {
-  std::string user_name;            // имя пользователя
-  std::cout << "Enter your name: "; // вывод запроса
-  std::cin >> user_name;            // ввод имени
+  std::string user_name;             // имя пользователя
+
+  std::cout << "Enter your name: ";  // вывод запроса
+  std::cin >> user_name;             // ввод имени
+
   std::cout << "Hello world from @" << user_name
-            << std::endl; // вывод приветствия
-  return 0;               // завершение программы
+            << std::endl;            // вывод приветствия
+
+  return 0;                          // завершение программы
 }
